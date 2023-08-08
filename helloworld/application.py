aee4055274850861a553139b1a3c15606ce564ce
Yoavbl <97474272+Yoavbl@users.noreply.github.com>
2023-08-08 20:24:42 +0500

    App Code

--- helloworld/application.py
+++ helloworld/application.py
@@ -5,13 +5,24 @@ from helloworld.flaskrun import flaskrun
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+
+@app.route("/", methods=["GET"])
 def get():
-    return Response(json.dumps({'Output': 'Hello World Hassan'}), mimetype='application/json', status=200)
+    return Response(
+        json.dumps({"Output": "Hello World Hassan"}),
+        mimetype="application/json",
+        status=200,
+    )
 
-@app.route('/', methods=['POST'])
+
+@app.route("/", methods=["POST"])
 def post():
-    return Response(json.dumps({'Output': 'Hello World Hassan'}), mimetype='application/json', status=200)
+    return Response(
+        json.dumps({"Output": "Hello World Hassan"}),
+        mimetype="application/json",
+        status=200,
+    )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     flaskrun(app)
